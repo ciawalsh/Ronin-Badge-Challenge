220489043ca68c511e9cb133423c48d19818c8c1
ciawalsh <cia.walsh@gmail.com>
2015-01-09 15:27:19 +0000

    Front End completed. Need to add functionality now

--- lib/ronin_badge.rb
+++ lib/ronin_badge.rb
@@ -3,7 +3,29 @@ require 'sinatra/base'
 class Ronin_Badge < Sinatra::Base
   
   get '/' do
-    html :index
+    erb :index
+  end
+
+  post '/' do
+  	erb :index
+  	@name = params[:name]
+  end
+
+  post '/new_game' do
+  	@name = params[:name]
+  	erb :new_game
+  end
+
+  # get '/calculate' do
+  # 	@rock = params[:rock_button]
+  # end
+
+  get '/win' do
+  	erb :win
+  end
+
+  get '/lose' do
+  	erb :lose
   end
 
   # start the server if ruby file executed directly
